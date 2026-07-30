96c384bc344f29971da0f195ca3b512a060bc751
Keerti Billur <keertibillur5@gmail.com>
2026-07-30 13:02:06 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -5,5 +5,6 @@ public class WelcomeToJava {
         System.out.println("welcome to the java Track");
         System.out.println("I already know how to build programming logic.");
         System.out.println("Now I am learning how to express that logic using java.");
+        System.out.println("Now I know how to use git and github.");
     }
 }
